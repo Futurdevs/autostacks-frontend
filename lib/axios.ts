2802e8c5fd9b5c03bbaf216be3ece57b5eb4784c
Dave Mcsavvy <davemcsavvii@gmail.com>
2025-03-14 00:01:49 +0100

    Update axios baseURL assignment to use non-null assertion for improved type safety.

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getAuthToken } from './cookies';
 
-const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000';
+const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL!;
 
 const axiosInstance = axios.create({
   baseURL,
