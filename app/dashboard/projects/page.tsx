1849789c08d63c9f8214e8a7faae9d153ad2c8c1
orisabiyi <orisabiyidavid@gmail.com>
2025-03-14 19:12:39 +0100

    Enhance Projects page layout with a new button for creating projects and improved section structure

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Projects</h1>
-    </div>
+    <section>
+      <div>
+        <div className="flex items-center justify-between">
+          <h1>Projects</h1>
+          <Button className="shadow-neon-purple">Create a new project</Button>
+        </div>
+
+        <p>Here are the projects you are working on.</p>
+      </div>
+    </section>
   )
 }
