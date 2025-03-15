739ff1d39ef6db800e1739bc84baa3374acbd4af
orisabiyi <orisabiyidavid@gmail.com>
2025-03-15 09:01:15 +0100

    Refactor modal styling on Projects page for improved layout and navigation

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -24,8 +24,8 @@ export default function Page() {
 
       {showModal && (
         <figure className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-40 backdrop-blur-lg p-4 text-white shadow-lg h-full w-full z-50 flex items-center justify-center">
-          <section className="z-50 bg-gray-800 w-3/4 h-3/4 rounded-2xl p-8">
-            <nav>
+          <section className="z-50 bg-gray-800 w-3/4 h-3/4 rounded-2xl px-6 py-4">
+            <nav className="border border-gray-600 p-4 rounded-lg">
               <h2 className="font-normal text-white text-xl flex items-center gap-2">
                 <FaBrain className="inline-block text-purple-600" size={28} /> <span>Ask AI</span>
               </h2>
